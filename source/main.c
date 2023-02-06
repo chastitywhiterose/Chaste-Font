0f22952bbbd5ea6a9628eebf9acc1acc0da40c38
Chastity White Rose <chastitywhiterose@gmail.com>
2023-02-05 20:11:25 -0600

    Add files via upload

--- source/main.c
+++ source/main.c
@@ -13,10 +13,13 @@ int text_y; /*the y position of where text will go*/
 char text[0x200]; /*where text data is stored sometimes*/
 
 #include "ray_chastefont.h"
+#include "ray_chastefont_scaled.h"
 
 /* this function is now the official welcome screen*/
 void chaste_font_demo()
 {
+ text_x=font_8.char_height*1;
+ 
  
  /*before the game actually runs, optionally display a start screen*/
  while(!WindowShouldClose()) /*loop runs until key pressed*/
@@ -27,29 +30,36 @@ void chaste_font_demo()
   ClearBackground((Color){0,0,0,255});
 
   main_font=font_64;
-  text_x=main_font.char_height*1;
   text_y=main_font.char_height*1;
  
-  sprintf(text,"Chaste Font 64");
+  sprintf(text,"Font 64");
   chaste_font_draw_string(text,text_x,text_y);
 
   text_y+=main_font.char_height;
   main_font=font_32;
 
-  sprintf(text,"Chaste Font 32");
+  sprintf(text,"Font 32");
   chaste_font_draw_string(text,text_x,text_y);
 
   text_y+=main_font.char_height;
   main_font=font_16;
 
-  sprintf(text,"Chaste Font 16");
+  sprintf(text,"Font 16");
   chaste_font_draw_string(text,text_x,text_y);
 
   text_y+=main_font.char_height;
   main_font=font_8;
 
-  sprintf(text,"Chaste Font 8");
+  sprintf(text,"Font 8");
   chaste_font_draw_string(text,text_x,text_y);
+  
+  
+  text_y+=main_font.char_height*2;
+    sprintf(text,"Font Scaled");
+
+   chaste_font_draw_string_scaled(text,text_x,text_y,16);
+  
+  
 
   EndDrawing();
  }
---
+++ source/ray_chastefont_scaled.h
@@ -0,0 +1,40 @@
+/*chastity font scaled header*/
+
+
+/*
+ This function successfully draws a string of characters from the current loaded main font 
+ arguments are: character pointer, character x pos, character y pos, integer scale
+ 
+ It makes use of DrawTexturePro which is quite a complicated function capable of scaling as well as rotating textures.
+ Rotation makes no sense when outputting text which is meant to be readable.
+ But the scaling abilities of this function mean that it can multiply the font size by whatever you give it.
+ Therefore, an 8x8 character font such as the smallest one I have created can literally be scaled to any multiple of 8. So a scale of 3 would result in each character being 24x24.
+*/
+void chaste_font_draw_string_scaled(char *s,int cx,int cy,int scale)
+{
+ float rotation=0; /*this should always be zero*/
+
+ int x,y,i,c,cx_start=cx;
+ i=0;
+ while(s[i]!=0)
+ {
+  c=s[i];
+  if(c=='\n'){ cx=cx_start; cy+=main_font.char_height;}
+  else
+  {
+   x=(c-' ')*main_font.char_width;
+   y=0*main_font.char_height;
+   Rectangle rect_source = {x,y,main_font.char_width,main_font.char_height};
+   Rectangle rect_dest={ cx, cy, main_font.char_width*scale,main_font.char_height*scale };
+   Vector2 origin = {0,0};
+   DrawTexturePro(main_font.texture, rect_source, rect_dest, origin,rotation, WHITE);
+   cx+=main_font.char_width*scale;
+  }
+  i++;
+ }
+}
+
+
+
+
+
